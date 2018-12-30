e5724f24bb592b69e47999251dc833a1f5bc6e83
gnd <gnd@itchybit.org>
2018-12-30 23:44:30 +0100

    db settings fix (fck php)

--- db_class.php
+++ db_class.php
@@ -3,11 +3,15 @@
 class db {
     var $link;
     var $prfx;
+    var $db_table;
+    var $data_table;
 
     function connect() {
         include "sttngs.php";
         $this->db = new mysqli($db_host, $db_user, $db_pass, $db_name);
         $this->prfx = $db_prfx;
+        $this->db_table = $db_table;
+        $this->data_table = $data_table;
     }
 
     function close() {
@@ -15,12 +19,12 @@ class db {
     }
 
     function getCitySensors($city) {
-        $result = $this->db->query($kak = "SELECT id, name FROM {$db_table} WHERE city = '" . $city . "';");
+        $result = $this->db->query($kak = "SELECT id, name FROM {$this->db_table} WHERE city = '" . $city . "';");
         return $result;
     }
 
     function getLastMonthData($sensor_id) {
-        $result = $this->db->query("SELECT timestamp, pm10 from {$data_table} WHERE timestamp > DATE_SUB(now(), INTERVAL 30 DAY) AND timestamp <= now() and sensor_id = " . $sensor_id . ";");
+        $result = $this->db->query("SELECT timestamp, pm10 from {$this->data_table} WHERE timestamp > DATE_SUB(now(), INTERVAL 30 DAY) AND timestamp <= now() and sensor_id = " . $sensor_id . ";");
         return $result;
     }
 
@@ -30,12 +34,12 @@ class db {
             $sensors_selector .= "sensor_id = {$sensor} OR ";
         }
         $sensors_selector = substr($sensors_selector, 0, -4);
-        $result = $this->db->query("SELECT sensor_id, timestamp, pm10 from {$data_table} WHERE timestamp > DATE_SUB(now(), INTERVAL 30 DAY) AND timestamp <= now() and ({$sensors_selector});");
+        $result = $this->db->query("SELECT sensor_id, timestamp, pm10 from {$this->data_table} WHERE timestamp > DATE_SUB(now(), INTERVAL 30 DAY) AND timestamp <= now() and ({$sensors_selector});");
         return $result;
     }
 
     function getSensorInfo($sensor_id) {
-        $result = $this->db->query("SELECT city, name from {$db_table} WHERE id = " . $sensor_id . ";");
+        $result = $this->db->query("SELECT city, name from {$this->db_table} WHERE id = " . $sensor_id . ";");
         return $result;
     }
 
